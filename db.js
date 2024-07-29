cf516b7b310d783222761ca166142df3977a8294
gayatri <gayatribehere73@gmail.com>
2024-07-29 21:10:26 +0530

    second commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
 //const mongoURL="mongodb://localhost:27017/hotels";
-const mongoUrl=process.env.mongoDB_URL;
+const mongoURL=process.env.mongoDB_URL;
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express();
+
 const db=require('./db')
+
+const PORT=process.env.PORT||3000;
+
 const personRoutes=require('./routes/personRoutes')
 const menuRoutes=require('./routes/menuRoutes.js')
 const MenuITem=require('./models/MenuItem.js');
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
-
+require('dotenv').config();
 app.use('/person',personRoutes)
 
 app.use('/menu',menuRoutes)
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("yess")
 })
